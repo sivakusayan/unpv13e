e3024692176271c80595975e824967b562e9c630
Sayan Sivakumaran <sivakusayan@gmail.com>
2024-11-14 13:05:30 -0600

    Add .clangd file and suggested changes from exercises

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -15,20 +15,24 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
+	servaddr.sin_port   = htons(8000);	/* daytime server */
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
+    int counter = 0;
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+        counter += 1;
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
 			err_sys("fputs error");
 	}
 	if (n < 0)
 		err_sys("read error");
+
+    printf("Counter: %d\n", counter);
 
 	exit(0);
 }
